e0a1c17b5e3c4813408f99687a95ccb897b381e1
Ras azzab <ras.dinho7@gmail.com>
2020-03-09 02:35:08 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,22 +1,29 @@
+require 'pry'
+
 def generate_star_date
   (rand(100000) + 400000) / 10.0
+  binding.pry
 end
 
 
 def state_log(star_date)
   "Captain's Log, star date #{star_date}."
+  binding.pry
 end
 
 def crew
   ["Geordi", "Data", "Worf", "William", "Beverly", "Deanna"]
+  binding.pry
 end
 
 def greet_crew (crew)
   crew.each {|crew_member| "Hello #{crew_member}."}
+  binding.pry
 end
 
 def engage
   date = generate_star_date
   state_log(date)
   greet_crew(crew)
+  binding.pry
 end
