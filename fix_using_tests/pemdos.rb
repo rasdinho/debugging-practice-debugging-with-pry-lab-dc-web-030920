881c4245ff767c1a61207c30fa8c7c020a28ce37
Ras azzab <ras.dinho7@gmail.com>
2020-03-09 03:15:05 +0000

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,6 +1,18 @@
+require 'pry'
+
+# def snake_it_up(string)
+#   if string[0] == "s"
+#   10 * "s" + string
+#   "ssssssssss" + string
+#   # binding.pry
+#   else
+#   string
+#   end
+# end
 def snake_it_up(string)
   if string[0] == "s"
-  10 * "s" + string
+  # delete this line 10 * "s" + string
+  "ssssssssss" + string
   else
   string
   end
